1f5f319740bbc0821d74da31c28d1fce9c984017
Viraj Jagtap <124623303+virajjagtap2003@users.noreply.github.com>
2024-06-07 16:27:46 +0530

    Delete apibinding_get directory

--- apibinding_get/README.md
+++
@@ -1,3 +0,0 @@
-# apibinding_get
-
-![image](https://github.com/virajjagtap2003/Flutter_Basics/assets/124623303/06d35d3d-b63e-4672-a2e3-dce840183e7a)
--- apibinding_get/android/app/src/main/kotlin/com/example/apibinding_get/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.apibinding_get
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
